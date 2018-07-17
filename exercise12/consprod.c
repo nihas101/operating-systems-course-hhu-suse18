df08d4a5b0d8365c082ad4de31a37e696c190cbd
Nikita Hasert <nikitahasert@gmail.com>
2018-07-17 17:33:16 +0200

    Catch arguments that could possibly lead to deadlocks

--- exercise12/consprod.c
+++ exercise12/consprod.c
@@ -5,14 +5,16 @@
 #include <unistd.h>
 #include "queue.h"
 
+#define QUEUE_MAX_SIZE 100
+
 typedef struct arguments{
   queue_t *queue;
   int nr_of_items_per_cons;
   int nr_of_items_per_prod;
 } argument_t;
 
-sem_t mutex;
-sem_t filled;
+sem_t access_mutex;
+sem_t write_mutex, read_mutex;
 
 void join_threads(pthread_t* threads, int nr_of_threads);
 pthread_t* create_threads(argument_t *arguments, int nr_of_threads, void *function(void *args));
@@ -27,16 +29,22 @@ int main(int argc, char const *argv[]) {
   }
 
   // SETUP
-  pthread_t *prod_threads;
-  pthread_t *cons_threads;
-
   int nr_of_producers = atoi(argv[1]);
   int nr_of_consumers = atoi(argv[2]);
+  int nr_of_items = atoi(argv[3]);
 
-  queue_t *queue = queue_new(100);
+  if(nr_of_items % nr_of_consumers != 0 && nr_of_items % nr_of_producers != 0){
+    perror("Please provide a number of items (arg 3) that is divisible by the number of consumers (arg 2) and producers (arg 1).\n");
+    exit(EXIT_FAILURE);
+  }
 
-  sem_init(&mutex, 0, 1);
-  sem_init(&filled, 0, 0);
+  pthread_t *prod_threads;
+  pthread_t *cons_threads;
+  queue_t *queue = queue_new(QUEUE_MAX_SIZE);
+
+  sem_init(&access_mutex, 0, 1);
+  sem_init(&write_mutex, 0, 0);
+  sem_init(&read_mutex, 0, QUEUE_MAX_SIZE);
 
   argument_t *arguments = malloc(sizeof(argument_t));
   if(arguments == NULL){
@@ -45,8 +53,8 @@ int main(int argc, char const *argv[]) {
   }
 
   arguments->queue = queue;
-  arguments->nr_of_items_per_cons = atoi(argv[3])/nr_of_consumers;
-  arguments->nr_of_items_per_prod = atoi(argv[3])/nr_of_producers;
+  arguments->nr_of_items_per_cons = nr_of_items/nr_of_consumers;
+  arguments->nr_of_items_per_prod = nr_of_items/nr_of_producers;
 
   // THREAD EXECUTION
   cons_threads = (pthread_t *) create_threads(arguments, nr_of_consumers, consume);
@@ -56,8 +64,9 @@ int main(int argc, char const *argv[]) {
   join_threads(prod_threads, nr_of_producers);
 
   // CLEANUP
-  sem_destroy(&mutex);
-  sem_destroy(&filled);
+  sem_destroy(&access_mutex);
+  sem_destroy(&write_mutex);
+  sem_destroy(&read_mutex);
   free(cons_threads);
   free(prod_threads);
   free(arguments);
@@ -98,14 +107,15 @@ void *consume(void *args){
   int ret;
 
   for(int i=0; i < nr_of_items; i++){
-    sem_wait(&mutex);
+    sem_wait(&read_mutex);
+    sem_wait(&access_mutex);
     if(queue_pop_front(queue, &ret) >= 0){
       queue_print(queue);
     }else{
       i--;
     }
-    sem_post(&filled);
-    sem_post(&mutex);
+    sem_post(&write_mutex);
+    sem_post(&access_mutex);
   }
 
   return NULL;
@@ -116,14 +126,15 @@ void *produce(void *args){
   int nr_of_items = ((argument_t *) args)->nr_of_items_per_prod;
 
   for(int i=0; i < nr_of_items; i++){
-    sem_wait(&filled);
-    sem_wait(&mutex);
+    sem_wait(&write_mutex);
+    sem_wait(&access_mutex);
     if(queue_push_back(queue, i) >= 0){
       queue_print(queue);
     }else{
       i--;
     }
-    sem_post(&mutex);
+    sem_post(&read_mutex);
+    sem_post(&access_mutex);
   }
 
   return NULL;
